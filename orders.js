33db93eaed03a16a603be1016e473565706b16d1
Chainz <61576690+Chainz7@users.noreply.github.com>
2022-11-18 21:45:16 +0700

    Done

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+},
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+    {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+}
+]
